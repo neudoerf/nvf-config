2e3976d20c3f491ab9f038d1d98a0188481010eb
Tom Neudoerffer <tom@neudoerffer.com>
2025-01-21 22:19:35 -0500

    add fuzzy finding for telescope

--- config/picker.nix
+++ config/picker.nix
@@ -1,3 +1,9 @@
+{ pkgs, ... }:
 {
   vim.telescope.enable = true;
+  vim.lazy.plugins = {
+    "telescope-fzf-native.nvim" = {
+      package = pkgs.vimPlugins.telescope-fzf-native-nvim;
+    };
+  };
 }
